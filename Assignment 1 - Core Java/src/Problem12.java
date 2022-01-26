b845a8e293b6ed401a701d06cc2253e05305f386
AravindKrishnaR <aravind2000krishna@gmail.com>
2022-01-26 18:29:42 +0530

    Add problem 12

--- Assignment 1 - Core Java/src/Problem12.java
+++ Assignment 1 - Core Java/src/Problem12.java
@@ -2,28 +2,28 @@ import java.util.Scanner;
 
 public class Problem12 {
 	
+	private static boolean isPrime(int num) {
+		if (num <= 1)
+			return false;
+
+		for (int i = 2; i < num; i++)
+			if (num % i == 0)
+				return false;
+
+		return true;
+	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Enter the number: ");
-		int num = sc.nextInt(), flag = 0;
+		int num = sc.nextInt();
 		
-		if(num == 0 || num == 1) 
+		if(isPrime(num))
+			System.out.println("The number " + num + " is a PRIME NUMBER!");
+		else
 			System.out.println("The number " + num + " is a COMPOSITE NUMBER!");
 		
-		else {
-			for(int i = 2; i <= num/2; i++) {
-				if(num % i == 0) {
-					System.out.println("The number " + num + " is a COMPOSITE NUMBER!");
-					flag = 1;
-					break;
-				}
-			}
-			
-			if(flag == 0)
-				System.out.println("The number " + num + " is a PRIME NUMBER!");
-		}
-				
 		sc.close();
 	}
 }
