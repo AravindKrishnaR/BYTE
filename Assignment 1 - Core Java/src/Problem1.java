f3d6e878ee54693c789ecd50f8fbb69bbba78a41
AravindKrishnaR <aravind2000krishna@gmail.com>
2022-01-26 14:52:54 +0530

    Add problem1

--- Assignment 1 - Core Java/src/Problem1.java
+++ Assignment 1 - Core Java/src/Problem1.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
 
 public class Problem1 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	private static boolean isEven(int num) {
+		if(num % 2 == 0)
+			return true;
+		else
+			return false;
 	}
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter a number: ");
+		int num = sc.nextInt();
+		
+		if(isEven(num)) 
+			System.out.println("The number " + num + " is EVEN!");
+		else 
+			System.out.println("The number " + num + " is ODD!");
+		
+		sc.close();
+	}
 }
