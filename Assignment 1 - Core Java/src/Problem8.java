3a0adef39aab0d882279b203e9788055ae097139
AravindKrishnaR <aravind2000krishna@gmail.com>
2022-01-26 16:17:28 +0530

    Add problem 8

---
+++ Assignment 1 - Core Java/src/Problem8.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Problem8 {
+	
+	private static void pattern1(int num) {
+		for(int i = 1; i <= num; i++) {
+			for(int j = 1; j <= i; j++) {
+				System.out.print(i);
+			}
+			System.out.print("\n");
+		}
+	}
+
+	private static void pattern2(int num) {
+		for(int i = 1; i <= num; i++) {
+			for(int j = 1; j <= num; j++) {
+				System.out.print(num);
+			}
+			System.out.print("\n");
+		}
+	}
+
+	private static void pattern3(int num) {
+		for(int i = 1; i <= num; i++) {
+			for(int j = 1; j <= num-i; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("-----Pattern 1-----");
+		System.out.print("Enter a number: ");
+		int num = sc.nextInt();
+		pattern1(num);
+		
+		System.out.println("\n-----Pattern 2-----");
+		System.out.print("Enter a number: ");
+		num = sc.nextInt();
+		pattern2(num);
+		
+		System.out.println("\n-----Pattern 3-----");
+		System.out.print("Enter a number: ");
+		num = sc.nextInt();
+		pattern3(num);
+		
+		sc.close();
+	}
+}
