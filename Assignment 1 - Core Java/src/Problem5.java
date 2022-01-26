829c1239ae4f9e0743566b0be0d480fed8eaf03d
AravindKrishnaR <aravind2000krishna@gmail.com>
2022-01-26 15:46:29 +0530

    Add problem 5

--- Assignment 1 - Core Java/src/Problem5.java
+++ Assignment 1 - Core Java/src/Problem5.java
@@ -10,11 +10,12 @@ public class Problem5 {
 	
 	public static void main(String[] args) {
 		int num = 1;
+		
+		System.out.println("Even numbers from 1-50: ");
 		while(num <= 50) {
 			if(isEven(num))
 				System.out.print(num + " ");
 			num++;
 		}
 	}
-
 }
